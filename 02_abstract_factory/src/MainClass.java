e9ad9687505fdf756d7cedbaf798a659b72d3b5e
zhaole <383484192@qq.com>
2020-02-19 21:54:24 +0800

    增加单例模式

---
+++ 02_abstract_factory/src/MainClass.java
@@ -0,0 +1,16 @@
+public class MainClass {
+
+    public static void main(String[] args) throws Exception{
+
+        AbstractFactory abstractFactory1 = new AppleFactory();
+        abstractFactory1.getApple("AppleNorth").getFruit();
+        AbstractFactory abstractFactory2 = new AppleFactory();
+        abstractFactory2.getApple("AppleSouth").getFruit();
+        AbstractFactory abstractFactory3 = new BananaFactory();
+        abstractFactory3.getBanana("BananaNorth").getFruit();
+        AbstractFactory abstractFactory4 = new BananaFactory();
+        abstractFactory4.getBanana("BananaNorth").getFruit();
+
+    }
+
+}
